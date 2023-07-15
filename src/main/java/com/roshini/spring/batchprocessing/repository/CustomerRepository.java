0b207b2f63672840522d0712c5ae4c16abaee143
roshini <jeevanaroshini@gmail.com>
2023-07-15 19:51:45 +0530

    Revert "Spring Batch Processing"
    This reverts commit 47dc87ae38b1800a8add5067c7b7f06796870f2f.

--- src/main/java/com/roshini/spring/batchprocessing/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.roshini.spring.batchprocessing.repository;
-
-import com.roshini.spring.batchprocessing.entity.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository extends JpaRepository<Customer, Integer> {
-}
